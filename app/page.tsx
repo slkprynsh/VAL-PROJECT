b44d131af6c2c2207043b70f6a046345bb647318
slkprynshh <priann3652@gmail.com>
2026-06-24 20:10:23 +0530

    feat: remove TrustedBy and Stats sections from homepage completely

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 import { HeroSection } from '@/components/hero/hero-section';
-import { TrustedBySection } from '@/components/sections/trusted-by';
-import { StatsSection } from '@/components/sections/stats';
 import { TestimonialsSection } from '@/components/sections/testimonials';
 import { CTABanner } from '@/components/sections/cta-banner';
 
@@ -8,8 +6,6 @@ export default function Home() {
   return (
     <div className="flex flex-col">
       <HeroSection />
-      <TrustedBySection />
-      <StatsSection />
       <TestimonialsSection />
       <CTABanner />
     </div>
